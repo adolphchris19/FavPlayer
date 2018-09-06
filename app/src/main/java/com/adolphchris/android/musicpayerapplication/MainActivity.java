94c1f3132fb59199fa3dca4607359004e9b527a0
adolphchris19 <adolphchris19@gmail.com>
2018-09-06 05:45:13 -0700

    Progressing with the app, onclick listeners now working perfectly

--- app/src/main/java/com/adolphchris/android/musicpayerapplication/MainActivity.java
+++ app/src/main/java/com/adolphchris/android/musicpayerapplication/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    CardView songs;
+    CardView songs, artists, genres, albums;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         songs = findViewById(R.id.songs);
-
         songs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,5 +24,33 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        artists = findViewById(R.id.artists);
+        artists.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ArtistActivity.class);
+                startActivity(i);
+            }
+        });
+
+        genres = findViewById(R.id.genres);
+        genres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, GenreActivity.class);
+                startActivity(i);
+            }
+        });
+
+        albums = findViewById(R.id.albums);
+        albums.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, AlbumActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/adolphchris/android/musicpayerapplication/SongsActivity.java
+++ app/src/main/java/com/adolphchris/android/musicpayerapplication/SongsActivity.java
@@ -1,11 +1,14 @@
 package com.adolphchris.android.musicpayerapplication;
 
+import android.content.Intent;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -24,29 +27,38 @@ public class SongsActivity extends AppCompatActivity {
 
         //Creating the arraylist of the songs
         final ArrayList<MusicTemp> songs = new ArrayList<>();
-        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.images ));
-        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.images ));
-        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.images ));
-        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.images ));
-        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.images ));
-        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.images ));
-        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.images ));
-        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.images ));
-        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.images ));
-        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.images ));
-        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.images ));
-        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.images ));
-        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.images ));
-        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.images ));
-        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.images ));
-        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.images ));
+        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.songimageone ));
+        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.songimagetwo ));
+        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.songimagethree ));
+        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.songimagefour ));
+        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.songimageone ));
+        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.songimagetwo ));
+        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.songimagethree ));
+        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.songimagefour ));
+        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.songimageone ));
+        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.songimagetwo ));
+        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.songimagethree ));
+        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.songimagefour ));
+        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.songimageone ));
+        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.songimagetwo ));
+        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.songimagethree ));
+        songs.add(new MusicTemp("Holy is the Lord", "Chris", R.drawable.songimagefour ));
 
         //creating the adaPter that converts the arrays into views
         MusicAdapter adapter = new MusicAdapter(this, songs);
 
         final ListView listView = findViewById(R.id.list);
-
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                MusicTemp music = songs.get(position);
+                Intent i = new Intent(SongsActivity.this, NowPlayingActivity.class);
+                startActivity(i);
+            }
+        });
 
         //drawer navigation
         drawer = findViewById(R.id.drawer_layout);
